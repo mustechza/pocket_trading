2dc682aa9e082cd2d4a03fc78617456235a047a8
mustechza <141557155+mustechza@users.noreply.github.com>
2025-05-10 11:49:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,3 +90,11 @@ async def main_loop():
 
 # --- Run ---
 asyncio.run(main_loop())
+import requests
+
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = 'https://www.alphavantage.co/query?function=REALTIME_OPTIONS&symbol=IBM&apikey=AWKMJ8CFU7OUNCVG'
+r = requests.get(url)
+data = r.json()
+
+print(data)
