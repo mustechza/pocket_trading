d2be5a4aecdbc1826ed00c4223ae32a1ee0a3bb1
mustechza <141557155+mustechza@users.noreply.github.com>
2025-05-13 10:21:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,24 @@
+
+#API_KEY = "MhSFDGReh9WuilTZikVwW51OGujElIzOilRAoX7sgywPS4YMc5m0FQB67EWU0xfR"
+#API_SECRET = "j7BiDhZgKhaHIlPzNjv5KxQhwn3l0tWPGeVjUexNED4c3b3yEgoIwPMNgdR8nHi7"
 import requests
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 import os
-import pandas as pd
-#API_KEY = "MhSFDGReh9WuilTZikVwW51OGujElIzOilRAoX7sgywPS4YMc5m0FQB67EWU0xfR"
-#API_SECRET = "j7BiDhZgKhaHIlPzNjv5KxQhwn3l0tWPGeVjUexNED4c3b3yEgoIwPMNgdR8nHi7"
-api_key = os.getenv('MhSFDGReh9WuilTZikVwW51OGujElIzOilRAoX7sgywPS4YMc5m0FQB67EWU0xfR', '')
-api_secret = os.getenv('j7BiDhZgKhaHIlPzNjv5KxQhwn3l0tWPGeVjUexNED4c3b3yEgoIwPMNgdR8nHi7', '')
+
+api_key = os.environ['MhSFDGReh9WuilTZikVwW51OGujElIzOilRAoX7sgywPS4YMc5m0FQB67EWU0xfR']
+api_secret = os.environ['j7BiDhZgKhaHIlPzNjv5KxQhwn3l0tWPGeVjUexNED4c3b3yEgoIwPMNgdR8nHi7']
 
 url = 'https://api1.binance.com'
-api_call = '/api/v3/ticker/price'
-headers = {'content-type': 'application/json', 'X-MBX-APIKEY': api_key}
+# url = https://api.binance.us # for US users
 
-try:
-    response = requests.get(url + api_call, headers=headers)
-    response.raise_for_status()
-    data = response.json()
-    df = pd.DataFrame.from_records(data)
-    print(df.head())
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-    print("Network-related error occurred:", e)
-except requests.exceptions.RequestException as e:
-    print("HTTP error occurred:", e)
+api_call = '/api/v3/ticker/price'
+
+headers = {'content-type': 'application/json', 
+           'X-MBX-APIKEY': api_key}
+
+response = requests.get(url + api_call, headers=headers)
+
+response = json.loads(response.text)
+df = pd.DataFrame.from_records(response)
+df.head()
