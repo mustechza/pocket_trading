4cec248a99a46adbde768985ba8d6145cc934915
mustechza <141557155+mustechza@users.noreply.github.com>
2025-05-13 10:15:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,20 +2,22 @@ import requests
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 import os
+import pandas as pd
 
-api_key = os.environ['BINANCE_API_KEY_TEST']
-api_secret = os.environ['BINANCE_API_SECRET_TEST']
+api_key = os.getenv('BINANCE_API_KEY_TEST', '')
+api_secret = os.getenv('BINANCE_API_SECRET_TEST', '')
 
 url = 'https://api1.binance.com'
-# url = https://api.binance.us # for US users
-
 api_call = '/api/v3/ticker/price'
+headers = {'content-type': 'application/json', 'X-MBX-APIKEY': api_key}
 
-headers = {'content-type': 'application/json', 
-           'X-MBX-APIKEY': api_key}
-
-response = requests.get(url + api_call, headers=headers)
-
-response = json.loads(response.text)
-df = pd.DataFrame.from_records(response)
-df.head()
+try:
+    response = requests.get(url + api_call, headers=headers)
+    response.raise_for_status()
+    data = response.json()
+    df = pd.DataFrame.from_records(data)
+    print(df.head())
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+    print("Network-related error occurred:", e)
+except requests.exceptions.RequestException as e:
+    print("HTTP error occurred:", e)
